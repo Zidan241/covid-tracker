ed7d89206c931c40422f21438193a9c04caa380f
zidan241 <mohamedzidan241@gmail.com>
2022-04-16 02:19:41 +0000

    index js fix

--- server/index.js
+++ server/index.js
@@ -32,6 +32,9 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+if (process.env.NODE_ENV==='production') {
+  app.use(express.static('../client/build'));
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () =>{
   console.log(`Server up and running on port ${port}`);
