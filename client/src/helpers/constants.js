a405fb120d64d4162cde449f03db87dd0b409e54
zidan241 <mohamedzidan241@gmail.com>
2022-04-16 03:45:32 +0000

    Constant link set

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,6 +1,6 @@
 var tmp;
 if (process.env.NODE_ENV === "production"){
-  tmp = "https://www.animalsrepublic.org/api/";
+  tmp = "https://covid-tracker-nodogoro.herokuapp.com/api/";
 }
 else{
   tmp = "http://localhost:5000/api/"
